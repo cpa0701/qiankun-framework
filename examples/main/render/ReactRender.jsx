495c489bd9bb0772b1eb02feb98ec57576fbfa69
cpa0701 <121046856@qq.com>
2020-07-03 17:33:50 +0800

    first commit

--- examples/main/render/ReactRender.jsx
+++ examples/main/render/ReactRender.jsx
@@ -9,18 +9,13 @@ function Render(props) {
 
   return (
     <>
-      {loading && (
-        <h4 className="subapp-loading">Loading...</h4>
-      )}
+      {loading && <h4 className="subapp-loading">Loading...</h4>}
       <div dangerouslySetInnerHTML={{ __html: appContent }} />
     </>
-  )
+  );
 }
 
 export default function render({ appContent, loading }) {
   const container = document.getElementById('subapp-container');
-  ReactDOM.render(
-    <Render appContent={appContent} loading={loading} />,
-    container,
-  )
+  ReactDOM.render(<Render appContent={appContent} loading={loading} />, container);
 }
